e121c9e5144ff4c7df3ea24058a182c3d4e1acac
MarouaneDmy <marouane.demay@gmail.com>
2021-10-20 14:20:50 +0200

    localStorage bug

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -16,7 +16,7 @@ export default class {
     new Logout({ document, localStorage, onNavigate })
   }
 
-  handleClickNewBill = e => {
+  handleClickNewBill = (e) => {
     this.onNavigate(ROUTES_PATH['NewBill'])
   }
 
